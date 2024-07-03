96760187835136b0735d23674025ee345097623a
David Valor <davidvalorwork@gmail.com>
2024-07-02 23:57:06 -0400

    Funcionando

---
+++ index.py
@@ -0,0 +1,22 @@
+from bottle import run, post, request, response, hook
+from facebook_scraper import get_profile
+
+
+@hook("after_request")
+def enable_cors():
+    response.headers["Access-Control-Allow-Origin"] = "*"  # Permite todas las fuentes
+    response.headers["Access-Control-Allow-Methods"] = "PUT, GET, POST, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = (
+        "Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"
+    )
+
+
+@post("/")
+def receive_link():
+    facebook_link = request.body.read().decode("utf-8")
+    print(facebook_link)
+    res = get_profile(facebook_link, cookies="cookies.txt", friends=True)
+    return res
+
+
+run(host="localhost", port=8080, debug=True, reloader=True)
